08e4bc72dee204dbb4c622ad3a3091d1e3ae0001
Maria Budzanowska <budzanowska.maria@gmail.com>
2021-11-03 10:01:14 +0100

    clean up and set the base for repository search component

---
+++ src/components/App.tsx
@@ -0,0 +1,12 @@
+import { Provider } from "react-redux";
+import { store } from "../state";
+import RepositoriesList from "./RepositoriesList";
+
+const App = () => (
+  <Provider store={store}>
+    <h1>Search for a package</h1>
+    <RepositoriesList/>
+  </Provider>
+);
+
+export default App;
